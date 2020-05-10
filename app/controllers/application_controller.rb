29fe7b84cc6e6fd68e5be3b37475a1eadd372d1d
Nicholas Keller <nic.keller328@gmail.com>
2020-05-10 21:55:54 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -25,9 +25,32 @@ class ApplicationController < Sinatra::Base
     erb :index
   end
 
+  get '/articles/:id' do
+    @article = Article.find(params["id"])
+    erb :show
+  end
 
+  get '/articles/:id/edit' do
+    @post = Article.find(params["id"])
+    erb :edit
+  end
 
+  patch '/articles/:id' do
+    id = params["id"]
+    new_params = {}
+    old_article = Article.find(id)
+    new_params[:title] = params["title"]
+    new_params[:content] = params["content"]
+    old_article.update(new_params)
 
+    redirect "/articles/#{id}"
+  end
+
+  delete '/articles/:id/delete' do
+    @article = Article.find(params["id"])
+    @article.destroy
+    erb :delete
+  end
 
 
 
