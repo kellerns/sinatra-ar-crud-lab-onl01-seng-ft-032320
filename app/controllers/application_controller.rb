14e92dd08d6f3a936ee008b4ef690a053285c7f0
Nicholas Keller <nic.keller328@gmail.com>
2020-05-10 23:19:52 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,7 +17,7 @@ class ApplicationController < Sinatra::Base
 
   post '/articles' do
     @article = Article.create(params)
-    redirect to '/articles/<%=@article.id%>}'
+    redirect to '/articles'
   end
 
   get '/articles' do
