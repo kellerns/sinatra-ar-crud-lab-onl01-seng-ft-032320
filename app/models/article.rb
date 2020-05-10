19abfd88d937f04ab6edb040079a7f947ac3268e
Nicholas Keller <nic.keller328@gmail.com>
2020-05-10 22:53:38 +0000

    Automatically backed up by Learn

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,17 @@
 #Placeholder for a model
 
 class Article < ActiveRecord::Base
+  attr_accessor :name, :title
+
+  @@all = []
+
+  def initialize(name, title)
+    @name = name
+    @title = title
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
 end
