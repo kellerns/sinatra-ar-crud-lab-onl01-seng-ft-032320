c707c9bec58e9709933303d2e7748402e21fb707
Nicholas Keller <nic.keller328@gmail.com>
2020-05-10 23:06:30 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -20,10 +20,10 @@ class ApplicationController < Sinatra::Base
     redirect to '/articles'
   end
 
-  # get '/articles' do
-  #   @articles = Article.all
-  #   erb :index
-  # end
+  get '/articles' do
+    @articles = Article.all
+    erb :index
+  end
 
   get '/articles/:id' do
     @article = Article.find_by_id(params[:id])
